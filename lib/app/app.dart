bfbe5b58a48b1b18f56e0fb704b1af2d0f58e88c
John Colani <91166301+johnhcolani@users.noreply.github.com>
2022-10-31 10:40:28 -0400

    app state

---
+++ lib/app/app.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+
+   MyApp._internal(); // private named constructor
+  int appState = 0;
+
+  // single instance --- singleton
+  static final MyApp instance =  MyApp._internal();
+
+  // factory for the class instance
+  factory MyApp()=>instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
